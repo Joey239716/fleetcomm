875a9decc920e4cc36a7a38afb556ce5ab79bae6
joey239716 <thisisjoeyliu@gmail.com>
2026-03-27 01:29:30 -0400

    feat: hardcoded SoMa map with haversine edge distances

--- build/CMakeFiles/fleetcomm.dir/DependInfo.cmake
+++ build/CMakeFiles/fleetcomm.dir/DependInfo.cmake
@@ -8,6 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/joeyliu/fleetcomm/src/car.cpp" "CMakeFiles/fleetcomm.dir/src/car.cpp.o" "gcc" "CMakeFiles/fleetcomm.dir/src/car.cpp.o.d"
   "/Users/joeyliu/fleetcomm/src/graph.cpp" "CMakeFiles/fleetcomm.dir/src/graph.cpp.o" "gcc" "CMakeFiles/fleetcomm.dir/src/graph.cpp.o.d"
   "/Users/joeyliu/fleetcomm/src/main.cpp" "CMakeFiles/fleetcomm.dir/src/main.cpp.o" "gcc" "CMakeFiles/fleetcomm.dir/src/main.cpp.o.d"
   )
