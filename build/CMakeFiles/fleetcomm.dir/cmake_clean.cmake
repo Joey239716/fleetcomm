06c0d096af2398d70b5192d56cf4eceb1aff697f
joey239716 <thisisjoeyliu@gmail.com>
2026-03-26 20:15:49 -0400

    feat: initial proejct structure and CMake build:

---
+++ build/CMakeFiles/fleetcomm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fleetcomm.dir/src/main.cpp.o"
+  "CMakeFiles/fleetcomm.dir/src/main.cpp.o.d"
+  "fleetcomm"
+  "fleetcomm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fleetcomm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
